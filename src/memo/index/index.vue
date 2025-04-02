69b8f803131bbeeb3bc244d989b15486c81b42e5
18380446946 <872610221@qq.com>
2025-04-02 16:16:33 +0800

    新增删除修改

--- src/memo/index/index.vue
+++ src/memo/index/index.vue
@@ -22,13 +22,43 @@ onBeforeMount(() => {
 })
 onMounted(() => {
   // debugger;
+  // addMemos();
+  getList();
+  // deleteMemos();
+  updateMemos();
+})
+const addMemos=()=>{
   ajax.post("/memos/add", {
-    title: "这是标题",
-    content: "这是内容"
+  title:"这是标题",
+  content:"这是内容"
   }).then((res) => {
     console.log("添加成功", res);
   })
-})
+}
+const getList=()=>{
+  ajax.get("/memos/list", {
+   page:1,
+   rows:20
+  }).then((res) => {
+    console.log("查询成功", res);
+  })
+}
+const deleteMemos=()=>{
+  ajax.post("/memos/delete", {
+  id:1
+  }).then((res) => {
+    console.log("删除成功", res);
+  })
+}
+const updateMemos=()=>{
+  ajax.post("/memos/update", {
+  id:2,
+  title:"这是标题22222",
+  content:"这是内容22222"
+  }).then((res) => {
+    console.log("删除成功", res);
+  })
+}
 
 </script>
 
