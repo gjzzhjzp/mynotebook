0073191c5871c965230ff2f9a259632b7f78435c
lanyunshan <872610221@qq.com>
2025-04-23 20:16:47 +0800

    提交

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,7 @@ declare module 'vue' {
     EmptyData: typeof import('./src/components/common/emptyData.vue')['default']
     Header: typeof import('./src/components/common/Header.vue')['default']
     Help: typeof import('./src/components/common/help.vue')['default']
-    List: typeof import('./src/components/account/list.vue')['default']
+    List: typeof import('./src/components/memo/list.vue')['default']
     NutButton: typeof import('@nutui/nutui-taro')['Button']
     NutConfigProvider: typeof import('@nutui/nutui-taro')['ConfigProvider']
     NutDatePicker: typeof import('@nutui/nutui-taro')['DatePicker']
